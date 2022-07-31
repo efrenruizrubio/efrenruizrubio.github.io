c59398cb736b9ff71c639cd90aafecd793e3c8d5
efrenruizrubio <efren282@outlook.es>
2022-07-31 02:46:36 -0500

    footer and tabs finished

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+document.getElementById("defaultOpen").click();
+
+function openWork(evt, workName){
+    var experienceSliderItem, experienceSliderButton;
+    experienceSliderItem = document.getElementsByClassName("experience__slider__item");
+    for(let i = 0; i < experienceSliderItem.length; i++){
+        experienceSliderItem[i].style.display = "none";
+    }
+    experienceSliderButton = document.getElementsByClassName("experience__slider__button");
+    for(let i = 0; i < experienceSliderButton.length; i++){
+        experienceSliderButton[i].className = experienceSliderButton[i].className.replace(" active", "");
+    }
+    document.getElementById(workName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
