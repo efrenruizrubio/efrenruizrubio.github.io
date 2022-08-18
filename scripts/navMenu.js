70d3456a472a262fc5a63813be9d7de846d8ca5d
efrenruizrubio <efren282@outlook.es>
2022-08-17 19:02:24 -0500

    :bug: [FIX]: Fixed an issue with the hamburger menu

--- scripts/navMenu.js
+++ scripts/navMenu.js
@@ -1,14 +1,14 @@
-if(window.innerWidth < 768){
-    const navToggle = document.querySelector('.nav-toggle');
-    const button = document.querySelector('.header__aside__button');
-    const items = Array.from(document.querySelectorAll('.header__aside__list__item'));
-    items.forEach(i => {
-        i.addEventListener('click', toggleMenu);
-    })
-    navToggle.addEventListener('click', toggleMenu);
-    button.addEventListener('click', toggleMenu);
+const navToggle = document.querySelector(".nav-toggle");
+const button = document.querySelector(".header__aside__button");
+const items = Array.from(
+	document.querySelectorAll(".header__aside__list__item"),
+);
+items.forEach((i) => {
+	i.addEventListener("click", toggleMenu);
+});
+navToggle.addEventListener("click", toggleMenu);
+button.addEventListener("click", toggleMenu);
 
-    function toggleMenu (){
-        document.body.classList.toggle('nav-open');  
-    }
+function toggleMenu() {
+	document.body.classList.toggle("nav-open");
 }
